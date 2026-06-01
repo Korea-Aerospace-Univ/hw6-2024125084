1ef29e1370c4181577aa401f163c0ee52733c33b
2024125084 <kmj35003500@gmail.com>
2026-06-01 16:09:11 +0900

    Update main.c

--- p2/main.c
+++ p2/main.c
@@ -4,10 +4,10 @@ int main(void)
 {
     /* 1. 입력받을 정수 개수와 두 배열 선언 */
     int n;
-    int a[20], b[]
+    int a[20], b[20]
     int *p, *q;
     /* 2. 배열에 저장할 정수의 개수 입력 */
-    scanf("%d," &n);
+    scanf("%d", &n);
     /* 3. 포인터를 이동시키며 첫 번째 배열 입력 */
     for (p = a; p < a + n; p++)
         scanf("%d", p);
